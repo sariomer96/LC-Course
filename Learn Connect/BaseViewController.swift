8a5dfb7a0d57b7b27f2b6e5a985f6a1a9d444271
omer <46028769+sariomer96@users.noreply.github.com>
2024-12-04 23:13:36 +0300

    fixed  -register and login page design

--- Learn Connect/BaseViewController.swift
+++ Learn Connect/BaseViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BaseViewController.swift
 //  Learn Connect
 //
 //  Created by Omer on 4.12.2024.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
 
+       
+    }
+    
+
+  
 
 }
-
